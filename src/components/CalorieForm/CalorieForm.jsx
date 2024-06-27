59feff40efee1c9964e0eafbe638f2030b5673fa
Dan Retegan <danretegan@yahoo.com>
2024-06-27 17:42:43 +0300

    responsive adjustment

--- src/components/CalorieForm/CalorieForm.jsx
+++ src/components/CalorieForm/CalorieForm.jsx
@@ -30,124 +30,120 @@ const CalorieForm = () => {
         Calculate your daily calorie intake right now
       </div>
 
-      <section className={styles.sectionRight}>
-        {/* Height */}
-        <div className={styles.formGroup}>
+      {/* Height */}
+      <div className={styles.formGroup}>
+        <label className={styles.label}>
+          Height *
+          <input
+            type="number"
+            name="height"
+            value={formData.height}
+            onChange={handleChange}
+            className={styles.input}
+            required
+          />
+        </label>
+      </div>
+
+      {/* Age */}
+      <div className={styles.formGroup}>
+        <label className={styles.label}>
+          Age *
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            className={styles.input}
+            required
+          />
+        </label>
+      </div>
+
+      {/* Current weight */}
+      <div className={styles.formGroup}>
+        <label className={styles.label}>
+          Current weight *
+          <input
+            type="number"
+            name="currentWeight"
+            value={formData.currentWeight}
+            onChange={handleChange}
+            className={styles.input}
+            required
+          />
+        </label>
+      </div>
+
+      {/* Desired weight */}
+      <div className={styles.formGroup}>
+        <label className={styles.label}>
+          Desired weight *
+          <input
+            type="number"
+            name="desireWeight"
+            value={formData.desireWeight}
+            onChange={handleChange}
+            className={styles.input}
+            required
+          />
+        </label>
+      </div>
+
+      {/* Blood type */}
+      <div className={styles.formGroup}>
+        <span className={styles.label}>Blood type *</span>
+        <div className={styles.radioGroup}>
           <label className={styles.label}>
-            Height *
             <input
-              type="number"
-              name="height"
-              value={formData.height}
+              type="radio"
+              name="bloodType"
+              value="1"
+              checked={formData.bloodType === '1'}
               onChange={handleChange}
               className={styles.input}
               required
             />
+            <span>0</span>
           </label>
-        </div>
-
-        {/* Age */}
-        <div className={styles.formGroup}>
           <label className={styles.label}>
-            Age *
             <input
-              type="number"
-              name="age"
-              value={formData.age}
+              type="radio"
+              name="bloodType"
+              value="2"
+              checked={formData.bloodType === '2'}
               onChange={handleChange}
               className={styles.input}
               required
             />
+            <span>A</span>
           </label>
-        </div>
-
-        {/* Current weight */}
-        <div className={styles.formGroup}>
           <label className={styles.label}>
-            Current weight *
             <input
-              type="number"
-              name="currentWeight"
-              value={formData.currentWeight}
+              type="radio"
+              name="bloodType"
+              value="3"
+              checked={formData.bloodType === '3'}
               onChange={handleChange}
               className={styles.input}
               required
             />
+            <span>B</span>
           </label>
-        </div>
-      </section>
-
-      <section className={styles.sectionLeft}>
-        {/* Desired weight */}
-        <div className={styles.formGroup}>
           <label className={styles.label}>
-            Desired weight *
             <input
-              type="number"
-              name="desireWeight"
-              value={formData.desireWeight}
+              type="radio"
+              name="bloodType"
+              value="4"
+              checked={formData.bloodType === '4'}
               onChange={handleChange}
               className={styles.input}
               required
             />
+            <span>AB</span>
           </label>
         </div>
-
-        {/* Blood type */}
-        <div className={styles.formGroup}>
-          <span className={styles.label}>Blood type *</span>
-          <div className={styles.radioGroup}>
-            <label className={styles.label}>
-              <input
-                type="radio"
-                name="bloodType"
-                value="1"
-                checked={formData.bloodType === '1'}
-                onChange={handleChange}
-                className={styles.input}
-                required
-              />
-              <span>0</span>
-            </label>
-            <label className={styles.label}>
-              <input
-                type="radio"
-                name="bloodType"
-                value="2"
-                checked={formData.bloodType === '2'}
-                onChange={handleChange}
-                className={styles.input}
-                required
-              />
-              <span>A</span>
-            </label>
-            <label className={styles.label}>
-              <input
-                type="radio"
-                name="bloodType"
-                value="3"
-                checked={formData.bloodType === '3'}
-                onChange={handleChange}
-                className={styles.input}
-                required
-              />
-              <span>B</span>
-            </label>
-            <label className={styles.label}>
-              <input
-                type="radio"
-                name="bloodType"
-                value="4"
-                checked={formData.bloodType === '4'}
-                onChange={handleChange}
-                className={styles.input}
-                required
-              />
-              <span>AB</span>
-            </label>
-          </div>
-        </div>
-      </section>
+      </div>
 
       <button type="submit">Start losing weight</button>
     </form>
