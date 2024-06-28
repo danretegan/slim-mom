5a5c573cd6d28fe4eb44e684aef7a07204ce0465
Dan Retegan <danretegan@yahoo.com>
2024-06-28 15:50:52 +0300

    desktop styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Header.module.css';
 import logoImg from '../../images/logo.png';
 import slimMomImg from '../../images/slimMom.png';
+import logoDesktop from '../../images/logo-desktop.png';
 
 const Header = () => {
   return (
@@ -9,8 +10,14 @@ const Header = () => {
       <div className={styles.logo}>
         <img src={logoImg} alt="Logo" className={styles.logoImg} />
         <img src={slimMomImg} alt="Slim Mom" className={styles.slimMomImg} />
+        <img
+          src={logoDesktop}
+          alt="Logo Desktop"
+          className={styles.logoDesktop}
+        />
       </div>
       <nav className={styles.nav}>
+        <span className={styles.separator}></span>
         <a href="/login" className={styles.link}>
           LOG IN
         </a>
