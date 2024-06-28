8ed155fe05354f0ab08cbd7bd087a29aa0ec920f
Dan Retegan <danretegan@yahoo.com>
2024-06-28 13:15:46 +0300

    add SlimMom icon for tablet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
-import logoImg from '../../images/logo.png'; 
+import logoImg from '../../images/logo.png';
+import slimMomImg from '../../images/slimMom.png';
+
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img src={logoImg} alt="Logo" className={styles.logoImg} />{' '}
+        <img src={logoImg} alt="Logo" className={styles.logoImg} />
+        <img src={slimMomImg} alt="Slim Mom" className={styles.slimMomImg} />
       </div>
       <nav className={styles.nav}>
         <a href="/login" className={styles.link}>
