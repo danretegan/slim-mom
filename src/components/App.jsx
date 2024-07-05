b5463f909b909fb42035f001bccdefc4e323fedf
Dan <danretegan@yahoo.com>
2024-07-05 06:57:55 +0300

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
 import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
-import AuthProvider, { AuthContext } from '../context/AuthContext';
-
-const PrivateRoute = ({ element: Element, ...rest }) => {
-  const { auth } = React.useContext(AuthContext);
-  return auth.isAuthenticated ? (
-    <Element {...rest} />
-  ) : (
-    <Navigate to="/login" />
-  );
-};
+import AuthProvider from '../context/AuthContext';
 
 const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter basename="/slim-mom">
         <Routes>
-          <Route path="/" element={<PrivateRoute element={HomePage} />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/registration" element={<RegistrationPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<Navigate to="/" />} />
