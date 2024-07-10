8cf4e0b8a5634106bb6748e5f5440d86a7a6c870
Dan <danretegan@yahoo.com>
2024-07-10 16:13:56 +0300

    Reapply "App & index.js refactoring"
    This reverts commit 6f8ecc4430b627e112eb5e032295e2ec11542def.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
 import RegistrationPage from '../pages/RegistrationPage/RegistrationPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import DiaryPage from '../pages/DiaryPage/DiaryPage';
-import DiaryAddProductForm from '../components/DiaryAddProductForm/DiaryAddProductForm';
-import AuthProvider from '../context/AuthContext';
-import { BloodTypeProvider } from '../context/BloodTypeContext';
+import Calculator from '../pages/Calculator/Calculator';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <BloodTypeProvider>
-        <BrowserRouter basename="/slim-mom">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/registration" element={<RegistrationPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/diary" element={<DiaryPage />} />
-            <Route path="/add-product" element={<DiaryAddProductForm />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </BrowserRouter>
-      </BloodTypeProvider>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/registration" element={<RegistrationPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/diary" element={<DiaryPage />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
   );
 };
 
