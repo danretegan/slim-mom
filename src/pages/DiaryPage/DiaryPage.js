db80020c2abe4036dda82a89525a36b8e14b1dd9
Dan <danretegan@yahoo.com>
2024-07-12 09:14:24 +0300

    save item on server

--- src/pages/DiaryPage/DiaryPage.js
+++ src/pages/DiaryPage/DiaryPage.js
@@ -1,25 +1,65 @@
-import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
 import DiaryDateCalendar from 'components/DiaryDateCalendar/DiaryDateCalendar';
 import DiaryProductsList from 'components/DiaryProductsList/DiaryProductsList';
 import Header from 'components/Header/Header';
 import Button from 'components/Button/Button';
 import DiaryAddProductForm from 'components/DiaryAddProductForm/DiaryAddProductForm';
+import { AuthContext } from '../../context/AuthContext';
 import styles from './DiaryPage.module.css';
 
 const DiaryPage = () => {
+  const { auth } = useContext(AuthContext);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [products, setProducts] = useState([]);
   const [isAddProductFormOpen, setIsAddProductFormOpen] = useState(false);
-  // const navigate = useNavigate();
 
   const handleAddProduct = () => {
     setIsAddProductFormOpen(true);
   };
 
-  const handleSaveProduct = product => {
-    setProducts(prevProducts => [...prevProducts, product]);
-    setIsAddProductFormOpen(false);
+  const handleSaveProduct = async product => {
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/api/products/consumed',
+        {
+          productId: product._id,
+          date: selectedDate,
+          quantity: product.grams,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${auth.token}`,
+          },
+        }
+      );
+
+      setProducts(prevProducts => [
+        ...prevProducts,
+        { ...product, _id: response.data._id },
+      ]);
+      setIsAddProductFormOpen(false);
+    } catch (error) {
+      console.error('Error saving consumed product:', error);
+    }
+  };
+
+  const handleDeleteProduct = async productId => {
+    try {
+      await axios.delete(
+        `http://localhost:3000/api/products/consumed/${productId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${auth.token}`,
+          },
+        }
+      );
+      setProducts(prevProducts =>
+        prevProducts.filter(product => product._id !== productId)
+      );
+    } catch (error) {
+      console.error('Error deleting consumed product:', error);
+    }
   };
 
   return (
@@ -31,7 +71,7 @@ const DiaryPage = () => {
           selectedDate={selectedDate}
           onDateChange={setSelectedDate}
         />
-        <DiaryProductsList products={products} />
+        <DiaryProductsList products={products} onDelete={handleDeleteProduct} />
         <Button
           type="button"
           text="+"
